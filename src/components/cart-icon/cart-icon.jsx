1e5e29a1655991792d78c7fbc2cd7321b9ed144b
asemnouri <64498291+asemnouri@users.noreply.github.com>
2020-06-22 22:02:42 +0300

    lecture 118

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -4,15 +4,23 @@ import './cart-icon.scss'
 import {connect} from 'react-redux'
 import {toggleCartHidden} from '../../Redux/cart/cartAction'
 
-const CartIcon =({toggleCartHidden})=>(
+const CartIcon =({toggleCartHidden,itemCounter})=>(
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
+        <span className='item-count'>{itemCounter}</span>
     </div>
 )
 // you havve to pass the  toggleCart to the cartIcon 
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
   });
+  //this mapStateToProps is used for the counter only
+  //this way makes the react rerender every time although we dont change the currentItems
+  //so we use memoization
+  const mapStateToProps =({cart:{currentItems}})=>({
+    itemCounter:currentItems.reduce((accQuantity,currentItem)=>(
+      accQuantity + currentItem.quantity  
+    ),0)
+  })
 
-export default connect(null,mapDispatchToProps)(CartIcon)
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon)
